3981256275e5b39355ba7eb14d634fd87fce1aa8
fasjeit <tom.makarow@gmail.com>
2018-08-29 10:49:32 +0300

    missing helper

---
+++ src/CryptoLabsService/Helpers/TransformHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace CryptoLabsService.Helpers
+{
+    using System.IO;
+    using System.Security.Cryptography;
+
+    public static class TransformHelper
+    {
+        public static byte[] PerformCryptography(ICryptoTransform cryptoTransform, byte[] data)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(data, 0, data.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+    }
+}
