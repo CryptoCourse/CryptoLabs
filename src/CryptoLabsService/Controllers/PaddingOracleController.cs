af31801cf68cec9bcc86ef93149bad8b7415aad7
Fasjeit <15075638+Fasjeit@users.noreply.github.com>
2019-11-28 09:49:09 +0300

    Дока на лабу padding oracle.

--- src/CryptoLabsService/Controllers/PaddingOracleController.cs
+++ src/CryptoLabsService/Controllers/PaddingOracleController.cs
@@ -48,7 +48,13 @@
                 //#Q_
                 if (challengeId == PaddingOracleController.debugFlag)
                 {
-                    plainText = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, };
+                    plainText = new byte[]
+                        {
+                            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 
+                            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 
+                            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 
+                            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+                        };
                 }
 
                 var plainTextWithMac = this.paddingOracleManger.ApplyMac(
@@ -108,7 +114,7 @@
                 }
                 catch (Exception ex)
                 {
-                    return ex.ToString();
+                    return ex.Message;
                 }
             }
         }
